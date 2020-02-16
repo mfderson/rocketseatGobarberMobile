0951852c363bd45946c67911daeba4e75177fcfc
Anderson Freitas <mfderson@gmail.com>
2020-02-16 11:54:50 -0300

    Adicionando o arquivo src/index.js e a dependência de dev do babel-plugin-root-import

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// Criado com o snippet rnfc
+// import { Container } from './styles';
+
+export default function App() {
+  return <View />;
+}
